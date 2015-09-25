dcab98d51516f92245affe5b63ded8f547bc70d3
herring <amabile5684@gmail.com>
2015-09-25 11:30:19 +0900

    my second commit

---
+++ dijkstra/dijkstra/dijkstra.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX_V 999
+int V;
+
+vector<pair<int, int>> adj[MAX_V];
+vector<int> dijkstra(int src){
+	vector<int> dist(V, INF);
+	
+}
+int main(){
+	
+	
+	// 정점의 개수
+	
+	// 그래프의 인접리스트. (연결된 정점 번호, 간선 가중치) 쌍을 담는다.
+	
+	
+}
+
